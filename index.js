dbea23474ff106227e63b3cf2b937562b2192ff9
github342466 <59500013+github342466@users.noreply.github.com>
2021-12-27 11:00:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const JDclient = require('./jd/JDclient')
+const PDDclient= require('./pdd/pdd-client')
+const TBclient = require('./tb/api/topClient').TopClient;
+module.exports = {JDclient,TBclient,PDDclient}
